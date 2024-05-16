1564dfb1d7eb999bf2d6f42cddb566c6b6390ed5
PreetiDiwakarGitHub <p208016@gmail.com>
2024-05-16 11:51:10 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,50 @@
+// queation 1 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// function takevalue(){
+//     let name=document.getElementById("name").value;
+//     let password=document.getElementById("password").value;
+//     document.write("User Name:-   "+ name+"<br>");
+//     document.write("Password :-  " + password + "<br>");
+// }
+
+
+
+// question  2
+
+
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', changeBackgroundColor);
+
+// function changeBackgroundColor() {
+//     let color = ['red', 'green', 'blue', 'orange', 'pink'];
+//     let randomColor = color[Math.floor(Math.random() * color.length)];
+//     document.body.style.background = randomColor;
+// }
+
+
+
+
+
+// question  3
+
+// let box1 = document.getElementById("box");
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', ch);
+
+// function ch() {
+//     let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D"];
+//     let randomNum1 = num[Math.floor(Math.random() * num.length)];
+//     let randomNum2 = num[Math.floor(Math.random() * num.length)];
+//     let randomNum3 = num[Math.floor(Math.random() * num.length)];
+//     let subarr=[randomNum1,randomNum2,randomNum3].join(' ');
+//     box1.innerHTML = subarr;
+// }
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/domquestion.git
+// git push -u origin main
+
